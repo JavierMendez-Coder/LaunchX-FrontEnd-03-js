9023796d90837fcd490f3a289f5a86c20a1ac2e4
Javier Méndez <javiermendez0299@gmail.com>
2022-03-16 01:14:45 -0700

    Add index.html and scripts/pokedex.js

---
+++ scripts/pokedex.js
@@ -0,0 +1,20 @@
+const fetchPokemon = async () => {
+  const pokemonName = document.getElementById("pokemonName");
+  const input = pokemonName.value.toLowerCase();
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${input}`;
+  const response = await fetch(url).then((response) => response);
+
+  if (!response.ok) {
+    loadImage();
+    return;
+  }
+
+  const data = await response.json();
+  loadImage(data.sprites.front_default);
+};
+
+const loadImage = (imageUrl = "") => {
+  const pokemonImg = document.getElementById("pokemonImg");
+  pokemonImg.src = imageUrl;
+};
